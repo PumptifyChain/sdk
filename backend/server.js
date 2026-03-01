f0bdb392407699bdd7b94fbba92af6d37e43faff
Pumptify <opahlokih@gmail.com>
2026-03-01 23:14:22 +0700

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
+
+const trackRoutes = require("./routes/tracks")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+res.send("Pumptify API Running")
+})
+
+app.use("/api/tracks", trackRoutes)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+console.log("Server running on port",PORT)
+})
